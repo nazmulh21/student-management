82e9b9bd68b8f677cd9131ca1c7bc813e86cc938
Nazmul Hasan <nazmul.decor.ict@gmail.com>
2026-06-28 00:31:15 +0600

    personnel saved

--- src/main/java/com/exam/school_management/personnel/service/PersonnelService.java
+++ src/main/java/com/exam/school_management/personnel/service/PersonnelService.java
@@ -5,6 +5,8 @@ import com.exam.school_management.personnel.repo.PersonnelRepo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class PersonnelService {
@@ -17,4 +19,6 @@ public class PersonnelService {
 public PersonnelInfo doSave(PersonnelInfo personnelInfo){
         return personnelRepo.save(personnelInfo);
 }
+
+
 }
