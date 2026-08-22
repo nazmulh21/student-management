951e3ff78914678e346d18fb4c64e73782a7ba54
Nazmul Hasan <nazmul.decor.ict@gmail.com>
2026-08-23 05:59:29 +0600

    some featue updated

--- src/main/java/com/exam/school_management/user/user/Controller/UserController.java
+++ src/main/java/com/exam/school_management/user/user/Controller/UserController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse; // 👈 রেসপন্স �
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -41,6 +42,7 @@ public class UserController {
         this.jwtUtil = jwtUtil;
     }
 
+    @PreAuthorize("hasAnyAuthority('ADD_USER')")
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserRegistrationDto registrationDto) {
         //System.out.println("data::"+registrationDto);
@@ -166,6 +168,7 @@ public class UserController {
         }
     }
 
+    @PreAuthorize("hasAnyAuthority('USER_LIST')")
     @GetMapping(value="/user/list",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getUserList(){
         return ResponseEntity.ok(userService.getUserList());
